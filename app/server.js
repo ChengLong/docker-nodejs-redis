489512b2495ec13352c42d92904dba75e734afb5
Cheng Long <clong@thoughtworks.com>
2016-05-24 17:14:18 +0800

    Restructure app with test

---
+++ app/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express'),
+      http = require('http'),
+      redis = require('redis'),
+      os = require('os');
+
+const client = redis.createClient(6379, 'redis');
+const app = express();
+
+app.get('/', function(req, res, next) {
+  client.incr('visits', function(err, visits) {
+    if(err) return next(err);
+    const response = {
+      servedBy: 'This request is served by ' + os.hostname(),
+      pageVisits: 'You have viewed this page ' + visits + ' times!',
+      serverTime: 'Server Time: ' + new Date().toISOString(),
+      message: 'This is a random message!'
+    };
+
+    res.status(200).json(response);
+  });
+});
+
+const appPort = 8080;
+http.createServer(app).listen(appPort, function() {
+  console.log('Listening on port ' + appPort);
+});
---
+++ test/test.js
@@ -0,0 +1,31 @@
+const test_server = require("supertest").agent("http://app:8080");
+const assert = require('assert');
+
+describe("this awesome app",function(){
+  it("should return json response",function(done){
+  	test_server
+      .get("/")
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("should have correct key values in response",function(done){
+  	test_server
+      .get("/")
+      .expect(function(res) {
+        assert.ok(res.body.servedBy.startsWith('This request is served by '));
+        assert.ok(res.body.pageVisits.startsWith('You have viewed this page '));
+        assert.ok(res.body.serverTime.startsWith('Server Time: '));
+        assert.equal(res.body.message, 'This is a random message!');
+      })
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
